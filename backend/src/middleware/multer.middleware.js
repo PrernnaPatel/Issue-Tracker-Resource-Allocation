bb9230c065227150d7095727d522bf13697a9cfc
Prerna Patel <156068800+PrernnaPatel@users.noreply.github.com>
2026-04-06 22:32:13 +0530

    Delete backend directory

--- backend/src/middleware/multer.middleware.js
+++
@@ -1,8 +0,0 @@
-import multer from "multer"
-import cloudinary from "../utils/cloudinary.js"
-import fs from "fs"
-
-const storage=multer.diskStorage({})
-const upload=multer({storage})
-
-export default upload
